3671f673e77d6c180bb7ae7ce8a7327c3a0d6425
sahik7 <sahikahsan7@gmail.com>
2024-01-15 11:54:18 +0600

    data fetched properly

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -6,7 +6,7 @@ import TodoFilter from "./TodoFilter";
 export default function TodoContainer() {
     // const { todos } = useAppSelector(state => state.todos)
 
-    const { data: todos } = useGetTodosQuery()
+    const { data: todos, isLoading, isError } = useGetTodosQuery(undefined)
     return (
         <div><div className="flex justify-between mb-5">
 
