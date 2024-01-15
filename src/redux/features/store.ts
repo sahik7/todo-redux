4a3d290100945c6d1dd3b5037d19ea90b631eecf
sahik7 <sahikahsan7@gmail.com>
2024-01-15 11:39:04 +0600

    baseApi configured with the store

--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -2,7 +2,9 @@ import { configureStore } from '@reduxjs/toolkit'
 import todoReducer from "./todoSlice";
 export const store = configureStore({
     reducer: {
+        [baseApi.reducerPath]: baseApi.reducer,
         todos: todoReducer
+
     },
 })
 
