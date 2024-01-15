73335744750ebdcd04d0c69da5a835bd8c4b5040
sahik7 <sahikahsan7@gmail.com>
2024-01-15 11:36:52 +0600

    getTodos created

---
+++ src/redux/api/api.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+
+export const baseApi = createApi({
+    reducerPath: 'baseApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+    endpoints: (builder) => ({
+        getTodos: builder.query<Pokemon, string>({
+            query: (name) => `pokemon/${name}`,
+        }),
+    }),
+})
